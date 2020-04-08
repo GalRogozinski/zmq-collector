7a5eedd6b8190ae46ce4f699dfa2d805ffb09ed3
galrogo <gal.rogozinski@iota.org>
2020-04-08 16:45:58 +0300

    fix errors

--- zmq-collector/log_solidity.py
+++ zmq-collector/log_solidity.py
@@ -4,20 +4,20 @@ import re
 context = zmq.Context()
 
 def subscribe_socket(url):
-    socket = context.socket(zmq.REQ)
-    socket.setsockopt(zmq.SUBSCRIBE, b'ast')
-    socket.setsockopt(zmq.SUBSCRIBE, b'cs')
-    socket.setsockopt(zmq.SUBSCRIBE, b'qss')
-    socket.connect(url)
-    poller = zmq.Poller()
-    poller.register(socket)
+    socket1 = context.socket(zmq.SUB)
+    socket1.setsockopt(zmq.SUBSCRIBE, b'ast')
+    socket1.setsockopt(zmq.SUBSCRIBE, b'cs')
+    socket1.setsockopt(zmq.SUBSCRIBE, b'qss')
+    socket1.connect(url)
+    poller1 = zmq.Poller()
+    poller1.register(socket1)
     search = re.search('tcp://(.*).devnet.iota.cafe:5556', url)
-    return [socket, poller, search.group(1)]
+    return [socket1, poller1, search.group(1)]
 
 
 sub_list = []
 
-sub_list.append(subscribe_socket("tcp://iri01.devnet.iota.cafe:5556"))
+# sub_list.append(subscribe_socket("tcp://iri01.devnet.iota.cafe:5556"))
 sub_list.append(subscribe_socket("tcp://iri02.devnet.iota.cafe:5556"))
 sub_list.append(subscribe_socket("tcp://bare01.devnet.iota.cafe:5556"))
 sub_list.append(subscribe_socket("tcp://bare02.devnet.iota.cafe:5556"))
@@ -32,7 +32,7 @@ while True:
         socks = dict(poller.poll())
         if socket in socks and socks[socket] == zmq.POLLIN:
             message = socket.recv()
-            print(name + ": " + message)
+            print(name + ': ' + str(message))
 
 
 
